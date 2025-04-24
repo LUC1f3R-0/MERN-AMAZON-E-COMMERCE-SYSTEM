1949fc932f1f3b1548c539b4f5418a2a6494ef5e
thushara-workspace-admin <thusharathilan123@gmail.com>
2025-04-24 23:57:46 +0530

    frontend updated

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,9 @@
+import amazon_logo from './amazon-logo-white.png'
+import cart_icon from './icons/cart-icon.png'
+
+const assets = {
+    amazon_logo,
+    cart_icon
+}
+
+export default assets
--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,16 +1,13 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
-import AdminDashboard from '../pages/dashboard/admin/AdminDashboard'
-import Home from '../pages/Home'
-import AdminLogin from '../pages/auth/admin/AdminLogin'
+import Home from '../pages/Home/Home'
+
 
 const AppRoutes = () => {
     return (
         <Routes>
             <Route path='/' element={<Home />} />
-            <Route path='/admin' element={<AdminDashboard />} />
-            <Route path='/admin/login' element={<AdminLogin />} />
-        </Routes>
+        </Routes >
     )
 }
 
